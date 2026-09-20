598ecf649e381404102ecc3615619d2c07c9de85
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-09-20 14:13:07 +0900

    feat: add snapshot testing option to `nodejs-test-reusable` workflow (#182)

---
+++ test/sample.test.js
@@ -0,0 +1,13 @@
+import test, { before } from "node:test";
+import process from "node:process";
+
+const nodeVersion = process.versions.node;
+
+test("snapshot", (t) => {
+  if (/^(16|18|20)/.test(nodeVersion)) {
+    t.skip(`Snapshot testing is unsupported in Node.js v${nodeVersion}.`);
+    return;
+  }
+
+  t.assert.snapshot({ value: true });
+});
